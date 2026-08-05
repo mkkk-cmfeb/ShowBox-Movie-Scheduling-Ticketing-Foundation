c6f5e0a0378b2ecd1fc64ddd3640eb1c03af4747
nishchaybenkecmfeb26-cpu <nishchay.benke.cmfeb26@gmail.com>
2026-08-05 20:15:44 +0530

    Added enable Scheduling and Seat lock Service

--- showbox-backend/src/main/java/com/showbox/ShowboxApplication.java
+++ showbox-backend/src/main/java/com/showbox/ShowboxApplication.java
@@ -2,10 +2,14 @@ package com.showbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling; // Import this
 
 @SpringBootApplication
+@EnableScheduling // Add this to activate the 60-second background job
 public class ShowboxApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(ShowboxApplication.class, args);
     }
+
 }
--- showbox-backend/src/main/java/com/showbox/service/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.showbox.service;
